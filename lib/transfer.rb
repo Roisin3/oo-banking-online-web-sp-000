d3b03167e92ea243044cdb60843ecab48733ef62
Michael Lynch <michael@michaelrlynch.com>
2019-09-26 01:19:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,6 +32,7 @@ class Transfer
     if valid? && sender.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.balance += amount
+      self.status = "reversed"
     end
 
   end
